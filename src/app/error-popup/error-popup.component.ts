c96643851784242c719b978f565f1c750057b288
EmotionIce <46056526+EmotionIce@users.noreply.github.com>
2023-07-19 19:09:43 +0200

    Add automatic error popup

--- src/app/error-popup/error-popup.component.ts
+++ src/app/error-popup/error-popup.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ErrorPopupService } from '../services/error-popup.service';
 
 @Component({
   selector: 'app-error-popup',
   templateUrl: './error-popup.component.html',
   styleUrls: ['./error-popup.component.scss']
 })
-export class ErrorPopupComponent {
+export class ErrorPopupComponent implements OnInit {
+  errorMessage: string | null = null;
 
+  constructor(private errorPopupService: ErrorPopupService) {}
+
+  ngOnInit(): void {
+    this.errorPopupService.getErrorMessage().subscribe(errorMessage => {
+      this.errorMessage = errorMessage;
+      if(errorMessage){
+        setTimeout(() => {
+          this.errorMessage = null;
+        }, 10000);
+      }
+    });
+  }
 }
--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -26,6 +26,7 @@ import {
   SettingsService,
   Settings
 } from './settings';
+import { ErrorPopupService } from './error-popup.service';
 
 @Injectable({
   providedIn: 'root' //so every other dependencies can access this service
@@ -35,7 +36,9 @@ export class BackendService {
   private baseUrl = 'http://localhost:5000'; //actual url will be 8080 apparently
   private settings: Settings;
 
-  constructor(private http: HttpClient, private settingsService: SettingsService) {
+  constructor(private http: HttpClient, 
+              private settingsService: SettingsService,
+              private errorPopupService: ErrorPopupService) {
     this.settings = settingsService.getSettings();
   }
 
@@ -159,16 +162,21 @@ export class BackendService {
 
   // helper function to handle backend errors
   private handleError(error: HttpErrorResponse) {
-    // TODO: add error popup component and display error method
+    let errorMessage: string;
 
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
+      errorMessage = error.error.message;
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+      errorMessage = `Backend returned code ${error.status}, body was: ${error.error}`;
     }
+    // Use the ErrorPopupService to display the error message
+    this.errorPopupService.setErrorMessage(errorMessage);
+
     // Return an observable with a user-facing error message.
     return throwError(() => new Error('Something bad happened; please try again later.'));
   }
