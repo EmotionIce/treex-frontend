77a95892b6e14b97e573e5c8bc5b31df55fdc5af
utlzc <utlzc@student.kit.edu>
2023-07-21 21:00:30 +0200

    Add defaultevents for clicking/hovering/doubleClicking on Nodes

--- src/app/tree-view/tree-view.component.ts
+++ src/app/tree-view/tree-view.component.ts
@@ -1,13 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { DiagramComponent, Diagram, NodeModel, ConnectorModel, SnapSettingsModel, LayoutModel,
-     DataSourceModel, TextModel, DecoratorModel, ShapeStyleModel, SnapConstraints, UserHandleModel} from '@syncfusion/ej2-angular-diagrams';
+     DataSourceModel, TextModel, DecoratorModel, ShapeStyleModel, SnapConstraints, UserHandleModel, NodeConstraints, DataBinding, IClickEventArgs, IDoubleClickEventArgs} from '@syncfusion/ej2-angular-diagrams';
 import { DataManager} from '@syncfusion/ej2-data';
 //import { DataBinding } from '@syncfusion/ej2-angular-diagrams';
 //Diagram.Inject(DataBinding);
 
 export interface ElementInfo{
-    Title: string
-    Parent: string
+    content: string
+    parentID: string
 }
 
 @Component({
@@ -28,47 +28,30 @@ export class TreeViewComponent {
     //Initializes data source
     // elementID might also be an important attribute...
     public treeData: object[] = [
-        {Title: "Einleitung", AllowChildren: "true" },
-        {Title: "Kapitel 1.1", Parent: "Einleitung", AllowChildren: "true"},
-        {Title: "Kapitel 1.1.1", Parent: "Kapitel 1.1", AllowChildren: "true"},
-        {Title: "Kapitel 1.1.2", Parent: "Kapitel 1.1", AllowChildren: "true"},
-        {Title: "Kapitel 1.1.3", Parent: "Kapitel 1.1", AllowChildren: "true"},
-        {Title: "Kapitel 2", Parent: "Einleitung", AllowChildren: "true"},
-        {Title: "Kapitel 3", Parent: "Einleitung", AllowChildren: "true"},
-        {Title: "Kapitel 3.1", Parent: "Kapitel 3", AllowChildren: "false"},
-        {Title: "Kapitel 3.2", Parent: "Kapitel 3", AllowChildren: "false"},
-        {Title: "Kapitel 3.21", Parent: "Kapitel 3", AllowChildren: "false"},
-        {Title: "Kapitel 3.22", Parent: "Kapitel 3", AllowChildren: "false"}
-    ];
+        {elementID: "000", content: "Einleitung", summary: "Text Zusammenfassung1"},
+        {elementID: "001", content: "Kapitel 1", parentID: "000", summary: "Text Zusammenfassung2"},
+        {elementID: "002", content: "Kapitel 1.1", parentID: "001", summary: "Text Zusammenfassung3"},
+        {elementID: "003", content: "Kapitel 1.2", parentID: "001", summary: "Text Zusammenfassung4"},
+        {elementID: "004", content: "Kapitel 1.3", parentID: "001", summary: "Text Zusammenfassung5"},
+        {elementID: "005", content: "Kapitel 2", parentID: "000", summary: "Text Zusammenfassung6"},
+        {elementID: "006", content: "Kapitel 3", parentID: "000", summary: "Text Zusammenfassung7"},
+        {elementID: "007", content: "Kapitel 3.1", parentID: "006", summary: "Text Zusammenfassung8"},
+        {elementID: "008", content: "Kapitel 3.2", parentID: "006", summary: "Text Zusammenfassung9"},
+        {elementID: "009", content: "Kapitel 3.2.1", parentID: "008", summary: "Text Zusammenfassung10"},
+        {elementID: "009", content: "Kapitel 3.2.2", parentID: "008", summary: "Text Zusammenfassung10"}];
 
-    /*
-    
-    public treeData: object[] = [
-        {id: "0", Title: "Einleitung", AllowChildren: "true", Summary: "...", content:... },
-        {Title: "Kapitel 1.1", Parent: "Einleitung", AllowChildren: "true"},
-        {Title: "Kapitel 1.1.1", Parent: "Kapitel 1.1", AllowChildren: "true"},
-        {Title: "Kapitel 1.1.2", Parent: "Kapitel 1.1", AllowChildren: "true"},
-        {Title: "Kapitel 1.1.3", Parent: "Kapitel 1.1", AllowChildren: "true"},
-        {Title: "Kapitel 2", Parent: "Einleitung", AllowChildren: "true"},
-        {Title: "Kapitel 3", Parent: "Einleitung", AllowChildren: "true"},
-        {Title: "Kapitel 3.1", Parent: "Kapitel 3", AllowChildren: "false"},
-        {Title: "Kapitel 3.2", Parent: "Kapitel 3", AllowChildren: "false"},
-        {Title: "Kapitel 3.21", Parent: "Kapitel 3", AllowChildren: "false"},s
-        {Title: "Kapitel 3.22", Parent: "Kapitel 3", AllowChildren: "false"}
-    ];
-
-    */
 
     public jsonDatasourceSettings: Object = {
-        id: "Title",
-        parentId: "Parent",
+        id: "elementID",
+        parentId: "parentID",
         dataSource: new DataManager(this.treeData as JSON[])
     }
+
 
     public layoutSettings: LayoutModel = {
         type: 'ComplexHierarchicalTree',
         //type: 'OrganizationalChart',
-        //orientation: 'TopToBottom',
+        //orientation: 'LeftToRight',
 
         //springLength: 1,
         //springFactor: 0.99,
@@ -80,13 +63,15 @@ export class TreeViewComponent {
     }
 
     
-    public RemoveGridLines: SnapSettingsModel = { constraints: SnapConstraints.None };
+    public RemoveGridLines: SnapSettingsModel = { constraints: SnapConstraints.None};
 
     public getNodeDefaults(defaultnode: NodeModel): NodeModel{
         defaultnode.height = 50;
         defaultnode.width = 100;
+        defaultnode.constraints = NodeConstraints.Default;
+        defaultnode.constraints &= ~NodeConstraints.Delete;
         defaultnode.annotations = [
-            {content: (defaultnode.data as ElementInfo).Title, style: { color: "white" }}
+            {content: (defaultnode.data as ElementInfo).content, style: { color: "white" }}
         ]
         defaultnode.expandIcon = {
             shape: "ArrowDown",
@@ -103,7 +88,7 @@ export class TreeViewComponent {
         }
 
         // When loading tree: only show children of root
-        if((defaultnode.data as ElementInfo).Parent == null){
+        if((defaultnode.data as ElementInfo).parentID == null){
             defaultnode.isExpanded = true;
         } else {
             defaultnode.isExpanded = false;
@@ -111,7 +96,7 @@ export class TreeViewComponent {
 
         //alert(defaultnode.id)
         defaultnode.style = {fill: '#048785', strokeColor: 'Transparent', strokeWidth: 2}
-
+        
         return defaultnode;
     }
 
@@ -124,27 +109,49 @@ export class TreeViewComponent {
     }
 
     public clicked(node: NodeModel){
-        if((node.data as ElementInfo).Title == "Einleitung"){
+        if((node.data as ElementInfo).content == "Einleitung"){
             alert("huray");
         }
     }
-
-    public emptyAlert(){
-        alert("")
-    }
-
-    public onOpenDialog = function (event: any): void {
-        let nodeID = event.element.id
-        if(nodeID != undefined){
-        alert('Node id:' + event.element.id+"___element ID:" + (event.element.data as ElementInfo).Title);
-      }
-    };
 
     public getConnectorDefaults(defaultconnector: ConnectorModel) : ConnectorModel{
         defaultconnector.type = 'Orthogonal';
         defaultconnector.style = { strokeColor: '#6f409f', strokeWidth: 2}
         //defaultconnector.targetDecorator = { style: { fill: '6f409f', strokeColor: '6f409f' }};
         return defaultconnector;
+    }
+
+    public onNodeClick = function (event: any): void {
+        let nodeID = event.element.id
+        if(nodeID != undefined){
+        alert('Node id:' + event.element.id+"___element ID:" + (event.element.data as ElementInfo).content);
+      } else {
+        alert("missed node")
+      }
+    };
+ 
+    public onNodeHover(args: any): void {
+        //if (args.actualObject instanceof DataBinding.Node) {
+          const node = args.actualObject;
+          if (node.annotations.length > 0) {
+            //alert(node.annotations.length)
+            //node.annotations[0].visibility = false;
+            //alert("...")
+        }
+      //  }
+      }
+
+      public emptyAlert(){
+        alert("")
+    }
+
+    public doubleClick(args: any) : void
+    {
+        if(args.source == null){
+            alert("is null");
+        } else {
+            alert((args['source'].data as ElementInfo).content)
+        }
     }
 
 
