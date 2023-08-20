5ac9db23a84b73043d83af9dcb1d6767e3ad7010
urban <shacochaoslol@gmail.com>
2023-08-20 03:44:05 +0200

    merge with master, remove unnecessary drop area directive

--- src/app/drop-area.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DropAreaDirective } from './drop-area.directive';
-
-describe('DropAreaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DropAreaDirective();
-    expect(directive).toBeTruthy();
-  });
-});
