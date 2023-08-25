916105dd0b8e196b86d55441004d0fc167df3c0d
utlzc <jeff.buki@gmail.com>
2023-08-25 17:07:12 +0200

    Small Update

--- src/app/tree-view/tree-view.component.ts
+++ src/app/tree-view/tree-view.component.ts
@@ -26,6 +26,11 @@ import { LatexRenderComponent } from '../latex-render/latex-render.component';
 import { Root } from '../models/root';
 import { Element } from '../models/element';
 
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense(
+  'Ngo9BigBOggjHTQxAR8/V1NGaF1cWGhAYVF3WmFZfV1gd19GY1ZVQWY/P1ZhSXxQdk1hW35bcHdRQ2lfUEM='
+);
+
 
 import { DataManager } from '@syncfusion/ej2-data';
 import { Parent } from '../models/parent';
@@ -140,7 +145,7 @@ export class TreeViewComponent {
     public createNewDiagram(): Diagram{
         let diagram = new Diagram({
             width: '100%',
-            height: '499px',
+            height: '600px',
             snapSettings: { constraints: SnapConstraints.None },
             // defines the parent/children relationship in the JSON (needed to create the tree) and holds the data of the tree.
             dataSourceSettings: {
@@ -151,8 +156,6 @@ export class TreeViewComponent {
         
             //Configures automatic layout
             layout: this.layoutSettings,
-
-            nodeTemplate: this.createNodeTemplate,
             mouseOver: this.onNodeHover,
             drop: this.dropElement,
             //dragEnter: this.emptyAlert,
@@ -167,29 +170,6 @@ export class TreeViewComponent {
           });
           return diagram;
         }
-
-      
-      public createNodeTemplate(obj: any){
-          alert("")
-          //let template = document.createElement("div");
-          //template.className = "custom-node";
-          //return template;
-          return "";
-      }
-      /*public createNodeTemplate(node: NodeModel) {
-          // Create a div element representing the custom node
-          let template = document.createElement("dive");
-          template.className = "custom-node";
-
-          
-          
-          // Optionally, set node content using data binding
-          if (node.annotations && node.annotations[0] && node.annotations[0].content) {
-              template.textContent = "node.annotations[0].content";
-          }
-          
-          return template; // Return the custom template element
-      }*/
     
     
     // receives a JSON-Tree as input and updates the diagram accordingly
@@ -220,7 +200,6 @@ export class TreeViewComponent {
     public moveElement(elementId: string, parentId: string, previousChildId: string | null){
         this.backendService.MoveElementTree(elementId, parentId, previousChildId).subscribe(
             (responseData: any) => {
-              console.log(responseData)
               let newTreeData = (responseData as ReceivedData).tree
               let processedData = this.processTreeData(newTreeData)
               this.generateNewTree(processedData)
@@ -447,290 +426,3 @@ export class TreeViewComponent {
         }
     }
   }
-
-  
-/*
-function nodeDefaults(defaultnode: NodeModel): NodeModel{
-    defaultnode.height = 80;
-    defaultnode.width = 150;
-
-    if((defaultnode.data as ElementInfo).content != null){
-    defaultnode.annotations = [
-        {content: (defaultnode.data as ElementInfo).content, style: { color: "white" }}
-    ]
-}
-    defaultnode.style = {fill: '#048785', strokeColor: 'Transparent', strokeWidth: 2}
-    return defaultnode;
-}
-
-function connectorDefaults(defaultconnector: ConnectorModel) : ConnectorModel{
-        defaultconnector.type = 'Orthogonal';
-        defaultconnector.style = { strokeColor: '#6f409f', strokeWidth: 2}
-        return defaultconnector;
-    }
-
-
-
-
-
-//(data as ElementInfo)
-
-import { Component } from '@angular/core';
-import {
-  Diagram,
-  NodeModel,
-  ConnectorModel,
-  LayoutAnimation,
-  DataBinding,
-  HierarchicalTree,
-  SnapConstraints,
-  IDoubleClickEventArgs,
-  NodeConstraints
-} from '@syncfusion/ej2-diagrams';
-
-import { DataManager } from '@syncfusion/ej2-data';
-Diagram.Inject(DataBinding, HierarchicalTree, LayoutAnimation);
-
-export interface ElementInfo {
-  content: string;
-}
-
-@Component({
-  selector: 'app-tree-view',
-  templateUrl: './tree-view.component.html',
-  styleUrls: ['./tree-view.component.scss']
-})
-
-
-export class TreeViewComponent {
-
-public diagram: Diagram|null = null;
-
-constructor(){
-}
-
-
-public Data2: Object[] = [
-  {
-    elementID: '000',
-    content: 'Leitung',
-    summary: 'Text Zusammenfassung1',
-    isLeaf: false,
-  },
-  {
-    elementID: '001',
-    content: 'Kapitel 1',
-    parentID: '000',
-    summary: 'Text Zusammenfassung2',
-    isLeaf: false,
-  }
-];
-
-
-  ngOnInit(){
-    this.diagram = new Diagram({
-    width: '100%',
-    height: '499px',
-    snapSettings: { constraints: SnapConstraints.None },
-    //configures data source settings
-    dataSourceSettings: {
-      //sets the fields to bind
-      id: 'elementID',
-      parentId: 'parentID',
-      dataSource: new DataManager(this.Data as any),
-      //binds the data with the nodes
-      // doBinding: (nodeModel: NodeModel, data: object, diagram: Diagram) => {
-      //    nodeModel.shape = { type: 'Text', content: (data as EmployeeInfo).Name };
-      // }
-    },
-
-    //Configures automatic layout
-    layout: {
-      type: 'HierarchicalTree',
-      orientation: "LeftToRight",
-      verticalSpacing: 30,
-      horizontalSpacing: 40,
-      enableAnimation: true,
-    },
-
-    doubleClick: this.clickOnDiagram,
-    //Defines the default node and connector properties
-    getNodeDefaults: this.nodeDefaults2,
-    getConnectorDefaults: this.connectorDefaults,
-  });
-    this.diagram.appendTo('#diagram');
-    console.log(this.diagram)
-  }
-
-
-  //sets node default value
-  public nodeDefaults(defaultnode: NodeModel): NodeModel {
-    //defaultnode.height = 80;
-    //defaultnode.width = 150;
-    //defaultnode.isExpanded = false;
-    defaultnode.style = {
-      fill: '#048785',
-      strokeColor: 'Transparent',
-      strokeWidth: 2,
-    };
-
-    if ((defaultnode.data as ElementInfo).content != null) {
-      defaultnode.annotations = [
-        {
-          content: (defaultnode.data as ElementInfo).content,
-          style: { color: 'white' },
-        },
-      ];
-    }
-    defaultnode.borderColor = '#3a6eb5';
-    defaultnode.backgroundColor = '#659be5';
-
-    //(obj.shape as TextModel).margin = { left: 5, right: 5, bottom: 5, top: 5 };
-    return defaultnode;
-  }
-
-  public nodeDefaults2(defaultnode: NodeModel): NodeModel {
-    defaultnode.height = 80;
-    defaultnode.width = 150;
-    
-
-    defaultnode.constraints = NodeConstraints.Default  /*| NodeConstraints.Tooltip | NodeConstraints.AllowDrop; // allow all default constraints as well as Dragg & Drop
-    defaultnode.constraints &= ~NodeConstraints.Delete; // do not allow to delete a Node
-    defaultnode.constraints &= ~NodeConstraints.Rotate; // do not allow to rotate a Node
-    //defaultnode.isExpanded = false;
-
-    if ((defaultnode.data as ElementInfo).content != null) {
-      defaultnode.annotations = [
-        {
-          content: (defaultnode.data as ElementInfo).content,
-          style: { color: 'white' },
-        },
-      ];
-    }
-    defaultnode.style = {
-      fill: '#048785',
-      strokeColor: 'Transparent',
-      strokeWidth: 2,
-    };
-
-     defaultnode.expandIcon = {
-        shape: "Minus",
-        height: 10,
-        // position on the node of the "expandIcon"
-        offset: { 
-            x: 0.5,
-            y: 0.15
-        }
-    }
-
-    defaultnode.collapseIcon = {
-        shape: "Plus"
-    }
-
-  
-
-    return defaultnode;
-  }
-
-  public connectorDefaults(defaultconnector: ConnectorModel): ConnectorModel {
-    defaultconnector.type = 'Orthogonal';
-    defaultconnector.style = { strokeColor: '#6f409f', strokeWidth: 2 };
-    return defaultconnector;
-  }
-
-  // tslint:disable-next-line:max-func-body-length
-  public Data: Object[] = [
-    {
-      elementID: '000',
-      content: 'Leitung',
-      summary: 'Text Zusammenfassung1',
-      isLeaf: false,
-    },
-    {
-      elementID: '001',
-      content: 'Kapitel 1',
-      parentID: '000',
-      summary: 'Text Zusammenfassung2',
-      isLeaf: false,
-    },
-    {
-      elementID: '002',
-      content: 'Kapitel 1.1',
-      parentID: '001',
-      summary: 'Text Zusammenfassung3',
-      isLeaf: true,
-    },
-    {
-      elementID: '003',
-      content: 'Kapitel 1.2',
-      parentID: '001',
-      summary: 'Text Zusammenfassung4',
-      isLeaf: true,
-    },
-    {
-      elementID: '004',
-      content: 'Kapitel 1.3',
-      parentID: '001',
-      summary: 'Text Zusammenfassung5',
-      isLeaf: true,
-    },
-    {
-      elementID: '005',
-      content: 'Kapitel 2',
-      parentID: '000',
-      summary: 'Text Zusammenfassung6',
-      isLeaf: true,
-    },
-    {
-      elementID: '006',
-      content: 'Kapitel 3',
-      parentID: '000',
-      summary: 'Text Zusammenfassung7',
-      isLeaf: false,
-    },
-    {
-      elementID: '007',
-      content: 'Kapitel 3.1',
-      parentID: '006',
-      summary: 'Text Zusammenfassung8',
-      isLeaf: true,
-    },
-    {
-      elementID: '008',
-      content: 'Kapitel 3.2',
-      parentID: '006',
-      summary: 'Text Zusammenfassung9',
-      isLeaf: true,
-    },
-    {
-      elementID: '009',
-      content: 'Kapitel 3.2.2',
-      parentID: '008',
-      summary: 'Text Zusammenfassung10',
-      isLeaf: true,
-    },
-  ];
-
-  public clickOnDiagram = (args: any) => {
-    console.log("show Diag:")
-    console.log(this.diagram)
-    if(this.diagram != null){
-      this.diagram.dataSourceSettings.dataSource = new DataManager(this.Data2 as any);
-    }
-    //alert((args['source'].data as ElementInfo).content)
-    //this.diagram.clear();
-    //this.diagram.dataBind();
-    //this.diagram.appendTo('#diagram')
-
-   
-    
-  }  
-
-  
-  
-
-  
-
-  
-}
-*/ 
